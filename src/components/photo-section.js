0c87be7405b8fbd6c89ba53cb54b66517898e63e
DickinsonMark <monoposon@comcast.net>
2019-04-01 18:01:43 -0600

    Videos 1-7

--- src/components/photo-section.js
+++ src/components/photo-section.js
@@ -7,23 +7,38 @@ export default class PhotoSection extends Component {
             <View>
                 {/* This is the User section */}
                 <View>
-                    <Image />
-                    <Text></Text>
+                    <Image 
+                        style={styles.userAvatar}
+                        source={{uri: 'https://www.icmagroup.org/assets/repo-faq-1-400x299.jpg'}} />
+                    <Text>Username</Text>
                 </View>
                 {/* This is the actual photo section */}
                 <View>
-                    <Image />
+                    <Image 
+                        style={styles.mainPhoto}
+                        source={{uri: 'https://upload.wikimedia.org/wikipedia/en/thumb/b/b7/OperationRepo.jpg/250px-OperationRepo.jpg'}} />
                 </View>
                 {/* This is the heart button */}
                 <View>
-                    <Button />
+                    <Button title='Heart'/>
                 </View>
                 {/* This is the caption section */}
                 <View>
-                    <Text></Text>
-                    <Text></Text>
+                    <Text>Username</Text>
+                    <Text>some caption</Text>
                 </View>
             </View>
         );
+    }
+}
+
+const styles = {
+    userAvatar: {
+        width: 50,
+        height: 50
+    },
+    mainPhoto: {
+        width: null,
+        height: 400
     }
 }
